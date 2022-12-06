9de0fde7413e8ffb13b22d8b41e3d9ed5d758750
AndiS82 <19schneiderandreas82@gmail.com>
2022-12-06 15:30:21 +0100

    arrayready

--- src/components/Pokedex/Pokedex.js
+++ src/components/Pokedex/Pokedex.js
@@ -5,26 +5,26 @@ function Pokedex() {
     const [pokemon, setPokemon] = useState([]);
 
     useEffect(() => {
-        fetch(`https://pokeapi.co/api/v2/pokemon/`)
+        fetch(`https://pokeapi.co/api/v2/pokemon?limit=151`)
             //fetch(`https://pokeapi.co/api/v2/type/`)
             .then((response) => response.json())
             .then((data) => {
-                setData(data)
+                setData(data.results)
+                console.log(data.results[0].name);
+            })
+            .then(() => {
+                data.map((singlePokemon) => {
+                    console.log(singlePokemon);
+
+                    fetch(`https://pokeapi.co/api/v2/pokemon/${singlePokemon.name}`)
+                        .then((response) => response.json())
+                        .then((pokemon) => {
+                            setPokemon(pokemon)
+                        })
+                })
             })
     }, [])
     console.log(data);
-
-    const pokeArray = () => {
-        data.map((singlePokemon) => {
-
-            fetch(`https://pokeapi.co/api/v2/pokemon/${singlePokemon.name}`)
-                .then((response) => response.json())
-                .then((pokemon) => {
-                    setPokemon(pokemon)
-                })
-        }
-        )
-    }
     console.log(pokemon)
 }
 
---
+++ src/components/Pokedex/Pokemon/Pokemon.js
@@ -0,0 +1,15 @@
+
+
+function Pokemon() {
+    return (
+        <div className="imgBox">
+            <img></img>
+            <section className="pokeData">
+                <p className="pokeNumber"></p>
+                <p className="pokeName"></p>
+            </section>
+        </div>
+    )
+}
+
+export default Pokemon;
