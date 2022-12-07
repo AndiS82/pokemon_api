df1391538439b0d4e72260f909395d985e0b249a
sofia28304 <sofia.vogelhaupt@yahoo.de>
2022-12-07 15:29:15 +0100

    test

--- src/App.js
+++ src/App.js
@@ -5,13 +5,13 @@ import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import Home from './pages/Home'
 import Menu from './pages/Menu'
 import Details from './pages/Details'
-import Navbar from './components/Navbar'
+// import Navbar from './components/Navbar'
 
 function App() {
   return (
     <div className="App">
       <Router>
-        <Navbar />
+        {/* <Navbar /> */}
         <Routes>
           <Route path="/" element={<Home />} />
           <Route path="Menu" element={<Menu />} />
