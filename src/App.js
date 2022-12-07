1d4bf2ccc7ff4e98606cc546f412ae726514e8d5
sofia28304 <sofia.vogelhaupt@yahoo.de>
2022-12-07 12:14:57 +0100

    beitrag

--- src/App.js
+++ src/App.js
@@ -1,14 +1,25 @@
 import React from 'react';
 import './App.css';
-import Pokedex from './components/Pokedex/Pokedex';
+// import Pokedex from './components/Pokedex/Pokedex';
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import Home from './pages/Home'
+import Menu from './pages/Menu'
+import Details from './pages/Details'
+import Navbar from './components/Navbar'
 
 function App() {
-
   return (
     <div className="App">
-      <Pokedex></Pokedex>
+      <Router>
+        <Navbar />
+        <Routes>
+          <Route path="/" element={<Home />} />
+          <Route path="Menu" element={<Menu />} />
+          <Route path="Details" element={<Details />} />
+          {/* <Pokedex></Pokedex> */}
+        </Routes>
+      </Router>
     </div>
   );
 }
-
 export default App;
