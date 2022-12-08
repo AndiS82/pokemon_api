3161eb64c3bcde0148b548ab80248a4ad91f98b5
janine7492 <janine.supercode@gmail.com>
2022-12-08 12:24:53 +0100

    styling fixes

--- src/components/Navbar/Navbar.js
+++ src/components/Navbar/Navbar.js
@@ -6,8 +6,6 @@ function Navbar(props) {
         <section>
             <Link to={props.link}>{props.image}</Link>
 
-            {/* <input id="input" type="text" placeholder="Enter Pokemon ID or Name" onChange={props.search} ></input> */}
-
             <button type="button" onClick={props.mood}>{ }</button>
         </section>
     )
--- src/components/Pokedex/Pokedex.js
+++ src/components/Pokedex/Pokedex.js
@@ -1,38 +1,17 @@
 import React, { useEffect, useState } from 'react';
 import Pokemon from './Pokemon/Pokemon';
-import Navbar from '../Navbar/Navbar'
 import burger from '../../img/burger.png'
-import mood from '../../img/mood.png'
 import { Link } from 'react-router-dom';
 
 
 function Pokedex() {
     const [data, setData] = useState([]);
-    const [navtog, setNavtog] = useState("shown")
     const [inputname, setInputname] = useState("")
-    const [navtog2, setNavtog2] = useState("hidden")
-    const [data2, setData2] = useState([])
     const [useAbleData, setuseAbleData] = useState();
 
-    // const NavChange = (() => {
-    //     (navtog === "shown" ? setNavtog("hidden") : setNavtog("shown"))
-    //         (navtog2 === "shown" ? setNavtog2("hidden") : setNavtog2("shown"))
-    //     if (document.getElementById("input").value !== "") {
-    //         // { onInput= (e) => setInputname(document.getElementById("input").e.target.value.toLowerCase()) }
-    //     }
-    // }
-    // )
-    // useEffect(() => {
-    //     fetch(`https://pokeapi.co/api/v2/pokemon/${inputname}/`)
-    //         .then((response) => response.json())
-    //         .then((poki) => {
-    //             setData2(poki.results)
-    //         })
-    // }, [inputname])
 
     useEffect(() => {
         fetch(`https://pokeapi.co/api/v2/pokemon?limit=151`)
-            //fetch(`https://pokeapi.co/api/v2/type/`)
             .then((response) => response.json())
             .then((eichhörnchen) => {
                 // durch setData(eichhörnchen.results) wird in das objekt "eichhörnchen" in ein Array "results" umgeschrieben
@@ -48,7 +27,6 @@ function Pokedex() {
 
         let length = (inputname).length;
         setuseAbleData(data.filter(el => el.name.slice(0, length).toLowerCase() === (inputname).toLowerCase().replaceAll(" ", "-")));
-        // console.log(useAbleData)
     }, [inputname])
 
     if (useAbleData === undefined) {
@@ -66,13 +44,9 @@ function Pokedex() {
 
 
             </section>
-            {/* <div className={navtog2}>
-                <Pokemon key={inputname} name={inputname} />
-            </div> */}
-            <div className="pokeWrapper">
+            <div className="pokeWrapper"
+                id="pokeWrapper">
                 {useAbleData.map((singlePokemon, index) => {
-                    // console.log(navtog)
-                    // console.log(singlePokemon)
                     return <Pokemon key={index} name={singlePokemon.name} />
                 })}
             </div>
